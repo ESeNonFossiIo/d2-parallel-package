f6b31c3cfe8a2a989b75b683d1a1d7be3194c970
Luca Heltai <luca.heltai@gmail.com>
2015-11-06 18:26:22 +0100

    Added modules ninja and deal2lkit

---
+++ deal2lkit.sh
@@ -0,0 +1,18 @@
+SRC=deal2lkit
+
+REV=`cd $SRC; git branch -v | grep \* | awk '{print $3}'`
+
+if [ ! -d $SRC/build ]; then
+    mkdir $SRC/build
+    fi
+
+    cd $SRC/build
+    DST_INST=$OPT_INST/$SRC-dev
+
+    cmake \
+    -GNinja \
+    -DCMAKE_INSTALL_PREFIX:PATH=$OPT_INST \
+    ..
+
+    ninja -j$NP install
+    # ctest -j40 -V -S ../tests/run_testsuite.cmake 
